ffbb46af5e451fa3c7a820eff12943d4b138ab7d
narkok <nar-ste@yandex.ru>
2020-11-02 01:48:07 +0300

    Эффекты пикселизации и дисторшна

---
+++ Assets/Scripts/Effects/Distortion.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(Renderer))]
+public class Distortion: MonoBehaviour
+{
+
+    public Transform targetTransform;
+
+
+    public MeshRenderer _renderer;
+
+
+    private void Start()
+    {
+        _renderer = GetComponent<MeshRenderer>();
+    }
+
+
+    void Update()
+    {
+        Vector3 pos = targetTransform.position;
+
+        float x = Mathf.Sin((pos.x * 10) % 10 / 10) / 60;
+        float y = Mathf.Sin((pos.y * 10) % 10 / 10) / 60;
+        float z = Mathf.Sin((pos.z * 10) % 10 / 10) / 60;
+
+        Vector4 position = new Vector4(x, y, z, 0);
+        Debug.Log(position);
+        foreach (Material m in _renderer.sharedMaterials)
+        {
+            m.SetVector("_Position", position);
+        }
+    }
+}
---
+++ Assets/Scripts/Effects/Pixelation.cs
@@ -0,0 +1,78 @@
+﻿#if UNITY_EDITOR
+using UnityEditor;
+#endif
+using UnityEngine;
+
+[ExecuteInEditMode]
+[RequireComponent(typeof(Camera))]
+public class Pixelation : MonoBehaviour
+{
+
+    public bool UseScale = false;
+    [Range(2, 16)] public float Scale = 6;
+
+    public bool UseAbsolute = true;
+    [Range(2, 2000)] public int Height = Screen.height;
+
+    private Camera cameraComponent;
+    private RenderTexture texture;
+
+
+    private void Start()
+    {
+        CreateTexture();
+    }
+
+
+    private void CreateTexture()
+    {
+        int width = Screen.width;
+        int height = Screen.height;
+
+        if (UseScale)
+        {
+            width = Mathf.RoundToInt(Screen.width / Scale);
+            height = Mathf.RoundToInt(Screen.height / Scale);
+        }
+
+        if (UseAbsolute)
+        {
+            height = Height;
+            width = Screen.width * height / Screen.height;
+        }    
+
+        texture = new RenderTexture(width, height, 2, RenderTextureFormat.ARGB32, RenderTextureReadWrite.Default);
+        texture.antiAliasing = 1;
+        cameraComponent = GetComponent<Camera>();
+    }
+
+
+    #if UNITY_EDITOR
+    private void Update()
+    {
+        if (EditorApplication.isPlaying) return;
+        CreateTexture();
+    }
+    #endif
+
+
+    private void OnPreRender()
+    {
+        if (cameraComponent == null) return;
+        cameraComponent.targetTexture = texture;
+    }
+
+
+    private void OnPostRender()
+    {
+        if (cameraComponent == null) return;
+        cameraComponent.targetTexture = null;
+    }
+
+
+    void OnRenderImage(RenderTexture src, RenderTexture dest)
+    {
+        src.filterMode = FilterMode.Point;
+        Graphics.Blit(src, dest);
+    }
+}
