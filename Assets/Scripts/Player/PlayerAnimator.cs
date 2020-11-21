fca93f59eea158d55d3ce5757e1064d92c2af048
narkok <nar-ste@yandex.ru>
2020-11-21 15:11:47 +0300

    Анимации бега и поворота

---
+++ Assets/Scripts/Player/PlayerAnimator.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+public class PlayerAnimator: MonoBehaviour
+{
+
+    [SerializeField]
+    private Animator _animator;
+
+    [SerializeField]
+    private float _turnMultiplier = 0.3f;
+    public float TurnAmount;
+
+    [SerializeField]
+    private float _forwardMultiplier = 0.4f;
+    public float ForwardAmount;
+
+
+    void Update()
+    {
+        if (_animator == null) return;
+        _animator.SetFloat("Forward", ForwardAmount * _forwardMultiplier, 0.15f, Time.deltaTime);
+        _animator.SetFloat("Turn", TurnAmount * _turnMultiplier, 0.25f, Time.deltaTime);
+    }
+}
--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -2,6 +2,7 @@
 
 [RequireComponent(typeof(CharacterController))]
 [RequireComponent(typeof(Stamina))]
+[RequireComponent(typeof(PlayerAnimator))]
 public class PlayerMovement: MonoBehaviour
 {
     private CharacterController _controller;
@@ -9,20 +10,21 @@ public class PlayerMovement: MonoBehaviour
     private Stamina _stamina;
     private Transform _transform;
     private PlayerInput _input;
+    private PlayerAnimator _playerAnimator;
 
     [Header("Movement")]
     [SerializeField]
-    private float _walkSpeed = 2;
+    private float _walkSpeed = 1.4f;
     [SerializeField]
-    private float _runSpeed = 3.5f;
+    private float _runSpeed = 3.4f;
     private float _currentSpeed = 0;
     [SerializeField]
-    private float _turnSmoothTime = 0.1f;
+    private float _turnSmoothTime = 0.15f;
     private float _turnSmoothVelocity;
 
     [Header("Running")]
     [SerializeField]
-    private float _staminaForRun = 0.15f;
+    private float _staminaForRun = 0.06f;
     private bool _isRunning = false;
 
     [Space]
@@ -35,6 +37,7 @@ public class PlayerMovement: MonoBehaviour
         _controller = GetComponent<CharacterController>();
         _stamina = GetComponent<Stamina>();
         _mainCamera = Camera.main.transform;
+        _playerAnimator = GetComponent<PlayerAnimator>();
         _transform = transform;
         _currentSpeed = _walkSpeed;
         _input = new PlayerInput();
@@ -59,15 +62,23 @@ public class PlayerMovement: MonoBehaviour
         Vector2 inputDir = _input.Player.Move.ReadValue<Vector2>();
         Vector3 direction = new Vector3(inputDir.x, 0f, inputDir.y);
         float magnitude = Mathf.Clamp(direction.magnitude, -1, 1);
+        
 
         if (magnitude >=0.1f)
         {
             float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + _mainCamera.eulerAngles.y;
             float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref _turnSmoothVelocity, _turnSmoothTime);
+            _playerAnimator.TurnAmount = angle - _transform.rotation.eulerAngles.y;
             _transform.rotation = Quaternion.Euler(0f, angle, 0f);
 
             Vector3 moveDir = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
-            _controller.Move(moveDir.normalized * magnitude * _currentSpeed * Time.deltaTime);
+            moveDir = moveDir.normalized * magnitude * _currentSpeed;
+            _playerAnimator.ForwardAmount = moveDir.magnitude;
+            _controller.Move(moveDir * Time.deltaTime);
+        } else
+        {
+            _playerAnimator.ForwardAmount = 0;
+            _playerAnimator.TurnAmount = 0;
         }
 
         _controller.Move(Vector3.down * Time.deltaTime * _gravity);
