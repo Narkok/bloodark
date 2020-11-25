76610f9eed861be06bd4661e228e5db17387613f
narkok <nar-ste@yandex.ru>
2020-11-25 21:09:34 +0300

    Автоисправление материала дерева

---
+++ Assets/Scripts/Environment/TreeMaterialFixer.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(MeshRenderer))]
+public class TreeMaterialFixer: MonoBehaviour
+{
+    [SerializeField]
+    private Material _barkMaterial;
+
+
+    private void Awake()
+    {
+        GetComponent<MeshRenderer>().material = _barkMaterial;
+    }
+}
